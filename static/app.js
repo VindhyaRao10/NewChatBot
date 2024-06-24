a50ffc9f40bd2683aaba5b8d6608f1a504d05d56
varunudb <varunudb@gmail.com>
2024-06-24 22:16:25 +0530

    Form for Chat

--- static/app.js
+++ static/app.js
@@ -93,9 +93,8 @@ class Chatbox {
         let optionsMessage = {
             name: "Blox Buddy",
             message: '<div class="welcome-options">' +
-                '<button class="welcome-options button" onclick="sendMessage(\'Know more about us\')">Know more about us</button>' +
-                '<button class="welcome-options button" onclick="sendMessage(\'Contact us\')">Contact us</button>' +
-                '<button class="welcome-options button" onclick="sendMessage(\'Help us get to know you better\')">Help us get to know you better</button>' +
+                '<button class="welcome-options button" onclick="sendMessage(\'Get to know us better\')">Get to know us better</button>' +
+                '<button class="welcome-options button" onclick="sendMessage(\'Get in touch with us\')">Get in touch with us</button>' +
                 '</div>'
             };
         this.messages.push(optionsMessage);
@@ -104,7 +103,7 @@ class Chatbox {
 
     handleOptionClick(option) {
         const chatbox = document.querySelector('.chatbox__support');
-        if (option === 'Know more about us') {
+        if (option === 'Get to know us better') {
             let msg = { name: "Blox Buddy", message: "Here's more about us. Choose an option to learn more:" };
             this.messages.push(msg);
     
@@ -113,19 +112,59 @@ class Chatbox {
                 message: '<div class="welcome-options">' +
                     '<button class="welcome-options button" onclick="sendMessage(\'About Infoblox\')">About Infoblox</button>' +
                     '<button class="welcome-options button" onclick="sendMessage(\'About Our Products\')">About Our Products</button>' +
-                    '<button class="welcome-options button" onclick="sendMessage(\'About Us\')">About Us</button>' +
+                    '<button class="welcome-options button" onclick="sendMessage(\'Help us get to know you better\')">Help us get to know you better</button>' +
                     '</div>'
                 };
             this.messages.push(moreOptionsMessage);
             this.updateChatText(chatbox);
+        } else if (option === 'Help us get to know you better') {
+            this.showForm(chatbox);
         } else {
             this.onSendButton(chatbox, option);
+        }
+    }
+
+    showForm(chatbox) {
+        let formMessage = {
+            name: "Blox Buddy",
+            message: `
+                <div class="form">
+                    <div class="form-group">
+                        <label for="name">Name:</label>
+                        <input type="text" id="name" name="name" class="form-input">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="text" id="email" name="email" class="form-input">
+                    </div>
+                    <button class="send__form__button" onclick="submitForm()">Submit</button>
+                </div>
+            `
+        };
+        this.messages.push(formMessage);
+        this.updateChatText(chatbox);
+    }
+    
+    submitForm() {
+        const name = document.querySelector('#name').value;
+        const email = document.querySelector('#email').value;
+
+        if (name && email) {
+            let thankYouMessage = { name: "Blox Buddy", message: `Thank you, ${name}! We have received your contact information.` };
+            this.messages.push(thankYouMessage);
+            this.updateChatText(document.querySelector('.chatbox__support'));
+        } else {
+            alert('Please fill out all fields.');
         }
     }
 }
 
 function sendMessage(message) {
     chatbox.handleOptionClick(message);
+}
+
+function submitForm() {
+    chatbox.submitForm();
 }
 
 const chatbox = new Chatbox();
